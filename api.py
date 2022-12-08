33da8b18c384ce345c74583d4e399f535dcba204
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-12-07 23:10:35 -0500

    started some work on api stuff

--- api.py
+++ api.py
@@ -4,16 +4,21 @@ from random import Random
 # For SuperHero api
 
 superhero_key = ""
-superherostarturl = "https://superheroapi.com/api/" + superhero_key
+superherostarturl = "https://superheroapi.com/api/" + superhero_key + "/"
 
-def chooserandcharacter():
-    
+def generatecharacterquestion():
+    idlist = range(1,732) #maybe modify this list later to remove certain characters from being chosen
+    charid = Random.choice(idlist)
+    req = requests.get(superherostarturl + charid)
+    info = req.json()
+    name = info['name']
+    img = info['image']['url']
 
 # For pokeapi
 
 pokestarturl = "https://pokeapi.co/api/v2/pokemon/"
 
-def chooserandpokemon():
+def generatepokemonquestion():
     pokeid = Random.nextint(1,906)
     req = requests.get(pokestarturl + pokeid)
     info = req.json()
